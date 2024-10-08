a2a7083d0c67f3cf60b1e598ccc70b4ea7f26561
Varsha Hindupur <hindupur.v@northeastern.edu>
2024-10-08 14:57:28 -0400

    setup modified

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
 import 'package:intl/intl.dart';
 
-
-
 void main() {
   runApp(MyApp());
 }
@@ -39,7 +37,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
 
 // This widget is the home page of your application. It is stateful, meaning
 // that it has a State object (defined below) that contains fields that affect
@@ -50,19 +48,19 @@ MyHomePage({Key key, this.title}) : super(key: key);
 // used by the build method of the State. Fields in a Widget subclass are
 // always marked "final".
 
-final String title;
+  final String title;
 
-@override
-_MyHomePageState createState() => _MyHomePageState();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
+// 0 for robot 1 for my response
 class _MyHomePageState extends State<MyHomePage> {
   void response(query) async {
-    AuthGoogle authGoogle = await AuthGoogle(
-        fileJson: "assets/service.json")
-        .build();
+    AuthGoogle authGoogle =
+        await AuthGoogle(fileJson: "assets/service.json").build();
     Dialogflow dialogflow =
-    Dialogflow(authGoogle: authGoogle, language: Language.english);
+        Dialogflow(authGoogle: authGoogle, language: Language.english);
     AIResponse aiResponse = await dialogflow.detectIntent(query);
     setState(() {
       messsages.insert(0, {
@@ -71,9 +69,8 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     });
 
-
     print(aiResponse.getListMessage()[0]["text"]["text"][0].toString());
-   }
+  }
 
   final messageInsert = TextEditingController();
   List<Map> messsages = List();
@@ -91,9 +88,10 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Container(
               padding: EdgeInsets.only(top: 15, bottom: 10),
-              child: Text("Today, ${DateFormat("Hm").format(DateTime.now())}", style: TextStyle(
-                fontSize: 20
-              ),),
+              child: Text(
+                "Today, ${DateFormat("Hm").format(DateTime.now())}",
+                style: TextStyle(fontSize: 20),
+              ),
             ),
             Flexible(
                 child: ListView.builder(
@@ -105,82 +103,65 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               height: 20,
             ),
-
             Divider(
               height: 5.0,
               color: Colors.greenAccent,
             ),
             Container(
-
-
               child: ListTile(
-
-                  leading: IconButton(
-                    icon: Icon(Icons.camera_alt, color: Colors.greenAccent, size: 35,),
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.camera_alt,
+                    color: Colors.greenAccent,
+                    size: 35,
                   ),
-
-                  title: Container(
-                    height: 35,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(
-                          15)),
-                      color: Color.fromRGBO(220, 220, 220, 1),
-                    ),
-                    padding: EdgeInsets.only(left: 15),
-                    child: TextFormField(
-                      controller: messageInsert,
-                      decoration: InputDecoration(
-                        hintText: "Enter a Message...",
-                        hintStyle: TextStyle(
-                            color: Colors.black26
-                        ),
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                      ),
-
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black
-                      ),
-                      onChanged: (value) {
-
-                      },
-                    ),
+                ),
+                title: Container(
+                  height: 35,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                   ),
-
-                  trailing: IconButton(
-
-                      icon: Icon(
-
-                        Icons.send,
-                        size: 30.0,
-                        color: Colors.greenAccent,
-                      ),
-                      onPressed: () {
-
-                        if (messageInsert.text.isEmpty) {
-                          print("empty message");
-                        } else {
-                          setState(() {
-                            messsages.insert(0,
-                                {"data": 1, "message": messageInsert.text});
-                          });
-                          response(messageInsert.text);
-                          messageInsert.clear();
-                        }
-                        FocusScopeNode currentFocus = FocusScope.of(context);
-                        if (!currentFocus.hasPrimaryFocus) {
-                          currentFocus.unfocus();
-                        }
-                      }),
-
+                  padding: EdgeInsets.only(left: 15),
+                  child: TextFormField(
+                    controller: messageInsert,
+                    decoration: InputDecoration(
+                      hintText: "Enter a Message...",
+                      hintStyle: TextStyle(color: Colors.black26),
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                    ),
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                    onChanged: (value) {},
+                  ),
+                ),
+                trailing: IconButton(
+                    icon: Icon(
+                      Icons.send,
+                      size: 30.0,
+                      color: Colors.greenAccent,
+                    ),
+                    onPressed: () {
+                      if (messageInsert.text.isEmpty) {
+                        print("empty message");
+                      } else {
+                        setState(() {
+                          messsages.insert(
+                              0, {"data": 1, "message": messageInsert.text});
+                        });
+                        response(messageInsert.text);
+                        messageInsert.clear();
+                      }
+                      FocusScopeNode currentFocus = FocusScope.of(context);
+                      if (!currentFocus.hasPrimaryFocus) {
+                        currentFocus.unfocus();
+                      }
+                    }),
               ),
-
             ),
-
             SizedBox(
               height: 15.0,
             )
@@ -195,60 +176,59 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget chat(String message, int data) {
     return Container(
       padding: EdgeInsets.only(left: 20, right: 20),
-
       child: Row(
-          mainAxisAlignment: data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
-          children: [
-
-            data == 0 ? Container(
-              height: 60,
-              width: 60,
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/robot.jpg"),
-              ),
-            ) : Container(),
-
-        Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Bubble(
-            radius: Radius.circular(15.0),
-            color: data == 0 ? Color.fromRGBO(23, 157, 139, 1) : Colors.orangeAccent,
-            elevation: 0.0,
-
-            child: Padding(
-              padding: EdgeInsets.all(2.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-
-                  SizedBox(
-                    width: 10.0,
+        mainAxisAlignment:
+            data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          data == 0
+              ? Container(
+                  height: 60,
+                  width: 60,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/robot.jpg"),
                   ),
-                  Flexible(
-                      child: Container(
-                        constraints: BoxConstraints( maxWidth: 200),
+                )
+              : Container(),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Bubble(
+                radius: Radius.circular(15.0),
+                color: data == 0
+                    ? Color.fromRGBO(23, 157, 139, 1)
+                    : Colors.orangeAccent,
+                elevation: 0.0,
+                child: Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Flexible(
+                          child: Container(
+                        constraints: BoxConstraints(maxWidth: 200),
                         child: Text(
                           message,
                           style: TextStyle(
                               color: Colors.white, fontWeight: FontWeight.bold),
                         ),
                       ))
-                ],
-              ),
-            )),
+                    ],
+                  ),
+                )),
+          ),
+          data == 1
+              ? Container(
+                  height: 60,
+                  width: 60,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/default.jpg"),
+                  ),
+                )
+              : Container(),
+        ],
       ),
-
-
-            data == 1? Container(
-              height: 60,
-              width: 60,
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/default.jpg"),
-              ),
-            ) : Container(),
-
-          ],
-        ),
     );
   }
 }
